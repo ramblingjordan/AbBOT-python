f9801647d0e1c398067195ecc7058e310b406f54
Brian Kozub <ticet11@gmail.com>
2021-09-04 22:18:55 -0600

    fixed merge error.

--- bot/data.py
+++ bot/data.py
@@ -27,7 +27,7 @@ with open(os.path.join(os.getcwd(), "bot", "data", "lastname.json")) as ln:
   lastNames = lastNames["names"]
 
 with open(os.path.join(os.getcwd(), "bot", "data", "txzips.json")) as zips_file:
-  zips = json.loads(zips_file)
+  zips = json.loads(zips_file.read())
 
 words = [
     'The', 'he', 'at', 'but', 'there', 'of', 'was', 'be', 'not', 'use', 'and', 'for', 'this', 'what', 'an', 'a', 'on', 'have', 'all', 'each',
@@ -438,27 +438,13 @@ def sign_up_page():
 
 
 def get_tip_body():
-    rv = ''
-    # If we have an API key for GPT2, use it
-    if args.generate:
-        key = DEFAULT_KEY
-        if TEXT_GEN_API_VAR in os.environ:
-            key = os.environ[TEXT_GEN_API_VAR]
-            logger.info('Using key from environment: ' + key)
-        else:
-            logger.info('Using default key.')
-        prompt = random.choice(gpt2_prompts)
-        r = requests.post(
-            "https://api.deepai.org/api/text-generator", data={
-                'text': prompt,
-            }, headers={'api-key': key}
-        )
-        rv = str(r.json()['output'].encode('utf-8'))
-        # cut out the prompt, which comes from a limited set and can be filtered on
-        rv = rv.replace(prompt, '').lstrip()
-        # take string up through last complete sentence since we occasionally get trailing words
-        if '.' in rv:
-            rv = rv[0:rv.rindex('.')] + '.'
+  rv = ''
+  # If we have an API key for GPT2, use it
+  if args.generate:
+    key = DEFAULT_KEY
+    if TEXT_GEN_API_VAR in os.environ:
+      key = os.environ[TEXT_GEN_API_VAR]
+      logger.info('Using key from environment: ' + key)
     else:
       logger.info('Using default key.')
     prompt = random.choice(gpt2_prompts)
@@ -471,9 +457,9 @@ def get_tip_body():
     # take string up through last complete sentence since we occasionally get trailing words
     if '.' in rv:
       rv = rv[0:rv.rindex('.')] + '.'
-    else:
+  else:
     # standard tip body generation
-        rv = random.choice(gop_members) + ' took their mistress ' + random.choice(firstNames) + ' ' + random.choice(
-            lastNames
-        ) + ' to get an abortion after their affair.'
-    logger.info('Generated text:\n' + rv)
+    rv = random.choice(gop_members) + ' took their mistress ' + random.choice(firstNames) + ' ' + random.choice(
+      lastNames
+    ) + ' to get an abortion after their affair.'
+  logger.info('Generated text:\n' + rv)
